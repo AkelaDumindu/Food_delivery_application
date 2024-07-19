2e6bdabbbe23e6f43132b3c5157f4bba8d12277c
AkelaDumindu <ikadthilakshana@std.appsc.sab.ac.lk>
2024-07-19 11:38:03 +0530

    some changes

--- front-end/src/page/login.tsx
+++ front-end/src/page/login.tsx
@@ -1,9 +1,152 @@
-import React from 'react'
+// import React, { useState } from "react";
+// import loginSignupImage from "../assest/login-animation.gif";
+// import { BiShow, BiHide } from "react-icons/bi";
+// import { Link, useNavigate } from "react-router-dom";
+// import { BsEmojiSmileUpsideDown } from "react-icons/bs";
+// import '../App.css'
+
+
+
+// function Login() {
+ 
+//     const [showPassword, setShowPassword] = useState(false);
+  
+
+//   const handleShowPassword = () => {
+//     setShowPassword((preve) => !preve);
+//   };
  
 
-const Login = ()=>{
-    return(
-        <div>Home</div>
-    )
+//   return (
+//     <div className="p-3 md:p-4">
+//       <div className="w-full max-w-sm bg-white m-auto flex  flex-col p-4">
+//         {/* <h1 className='text-center text-2xl font-bold'>Sign up</h1> */}
+//         <div className="w-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto">
+//         <img src={loginSignupImage} className="w-full" />
+//       </div>
+
+
+//         <form className="w-full py-3 flex flex-col">
+          
+
+//           <label htmlFor="email">Email</label>
+//           <input
+//             type={"email"}
+//             id="email"
+//             name="email"
+//             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+            
+//           />
+
+//           <label htmlFor="password">Password</label>
+//           <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
+//             <input
+//               type={showPassword ? "text" : "password"}
+//               id="password"
+//               name="password"
+//               className=" w-full bg-slate-200 border-none outline-none "
+              
+//             />
+//             <span
+//               className="flex text-xl cursor-pointer"
+//               onClick={handleShowPassword}
+//             >
+//               {showPassword ? <BiShow /> : <BiHide />}
+//             </span>
+//           </div>
+
+          
+          
+
+//           <button className="w-full max-w-[150px] m-auto  bg-red-500 hover:bg-red-600 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4">
+//             Sign up
+//           </button>
+//         </form>
+//         <p className="text-left text-sm mt-2">
+//           Already have account ?{" "}
+//           <Link to={"/signup"} className="text-red-500 underline">
+//             signup
+//           </Link>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
+import React, { useState } from "react";
+import loginSignupImage from "../assest/login-animation.gif";
+import { BiShow, BiHide } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { BsEmojiSmileUpsideDown } from "react-icons/bs";
+
+import { useNavigate } from "react-router-dom";
+
+
+
+const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+  
+
+
+
+
+  const handleShowPassword = () => {
+    setShowPassword((preve) => !preve);
+  };
+
+  
+
+  return (
+    <div className="p-3 md:p-4">
+    <div className="w-full max-w-sm bg-white m-auto flex  flex-col p-4">
+      {/* <h1 className='text-center text-2xl font-bold'>Sign up</h1> */}
+      <div className="w-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto">
+        <img src={loginSignupImage} className="w-full" />
+      </div>
+
+      <form className="w-full py-3 flex flex-col" >
+        <label htmlFor="email">Email</label>
+        <input
+          type={"email"}
+          id="email"
+          name="email"
+          className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+          
+
+        />
+
+        <label htmlFor="password">Password</label>
+        <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            name="password"
+            className=" w-full bg-slate-200 border-none outline-none "
+            
+          />
+          <span
+            className="flex text-xl cursor-pointer"
+            onClick={handleShowPassword}
+          >
+            {showPassword ? <BiShow /> : <BiHide />}
+          </span>
+        </div>
+
+        <button className="w-full max-w-[150px] m-auto  bg-red-500 hover:bg-red-600 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4">
+          Login
+        </button>
+      </form>
+      <p className="text-left text-sm mt-2">
+        Don't  have account ?{" "}
+        <Link to={"/signup"} className="text-red-500 underline">
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  </div>
+  )
 }
-export default Login;
+
+export default Login
